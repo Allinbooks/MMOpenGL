711896e59d64fd01b10d0c02171db90f51e388c4
Allinbooks <724990045@qq.com>
2022-06-09 22:34:02 +0800

    feat: 创建利用OpenGL创建窗口，其中涉及创建，绘制，循环判断关闭状态。

--- MMOpenGL/MMOpenGL.cpp
+++ MMOpenGL/MMOpenGL.cpp
@@ -13,9 +13,42 @@
 
 int main()
 {
+	// glfwInit();
+	// gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+	printf("Hello MMOpenGL!\n");
 	glfwInit();
+
+	// 设置版本号
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// TODO Core Profile.什么是CORE_PROFILE？
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	int windowWidth = 1280;
+	int windowHeight = 720;
+	char* windowTitle = "MMOpenGL";
+	GLFWwindow*  window = glfwCreateWindow(windowWidth, windowHeight, windowTitle, NULL, NULL);
+
+	/* OpenGL的运行模式 -（必须）- 单线程模式
+	 * 多个线程同时调用不被允许
+	 * 将当前线程和OpenGL Context做一个绑定
+	 * 如果出现多线程并行需要对资源上锁，这样的多线程并没有提升性能，因此不建议
+	 */
+	glfwMakeContextCurrent(window);
+
+	// 制作GLAD
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-	printf("Hello MMOpenGL!");
+	while (!glfwWindowShouldClose(window)) {
+		//TODO 绘制操作
+
+		//TODO 等到双缓冲之后再了解
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	printf("Close success!!!\n");
 	return 0;
 }
