5423e237e0bda07ef100dc5ea7f49ae907788a6f
Allinbooks <724990045@qq.com>
2022-06-09 13:09:39 +0800

    first commit

---
+++ MMOpenGL/MMOpenGL.cpp
@@ -0,0 +1,21 @@
+﻿// MMOpenGL.cpp: 定义应用程序的入口点。
+//
+#include <stdio.h>
+
+// 顺序有要求，GLFW依赖于glad
+#include <glad/glad.h>
+#include <GLFW/glfw3.h> 
+
+#include "MMOpenGL.h"
+
+
+
+
+int main()
+{
+	glfwInit();
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+	printf("Hello MMOpenGL!");
+	return 0;
+}
